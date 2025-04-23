ce8341dcc9244117f7463910cc276e340f0255f0
2nd.creator2025 <2nd.creator2025@gmail.com>
2025-04-23 12:44:02 +0000

    first commit

--- workersbuild.sh
+++ workersbuild.sh
@@ -29,8 +29,7 @@ tar xzvf docker.tgz --strip 1 -C /tmp/docker-install
 echo "Verifying Docker installation..."
 /tmp/docker-install/docker --version
 
-/tmp/docker-install/dockerd/dockerd-rootless-setuptool.sh install
-
+chmod +x ./tmp/docker-install/dockerd/dockerd-rootless-setuptool.sh && ./tmp/docker-install/dockerd/dockerd-rootless-setuptool.sh install
 
 # Step 4: Build and run the Docker container
 echo "Building and running Docker container..."
